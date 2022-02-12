e47f09b4234815c81659ef076dfcde61da27c6c0
hintjaz <ulechka.zel2004@outlook>
2022-02-12 12:48:46 +0200

    load

---
+++ src/com/outlook/ulechka/zel2004/Task.java
@@ -0,0 +1,10 @@
+package com.outlook.ulechka.zel2004;
+
+public class Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
